f2b3eaafa133f5d2f2f4c8575bfaae3a4d4a3987
juan <jcortesj@unal.edu.co>
2022-06-25 16:05:03 -0500

    problema de infinitos renders

--- pages/notificaciones.tsx
+++ pages/notificaciones.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../layout/Layout'
 import Notificaciones from '../components/Notificaciones';
 import Desplegable from '../components/notificacionDesplegable';
@@ -19,13 +19,17 @@ const notificaciones = () => {
   const {user, setUser} = useUser();
 const {auth, setAuth} = useAuth();
 const {admin, setAdmin} = useAdmin();
+
   
   let usuario:Usuario = user;
   let notificaciones:Notificacion[] = [];
+  const [contadorNotific,setNot] = useState(notificaciones.length);
 
   if(user != undefined){
     if(usuario instanceof Administrador){
       notificaciones = user.notificaciones;
+    
+      
       //usar geters y seters es un problema grave, mejor acceder al atributo
 
   
@@ -33,16 +37,25 @@ const {admin, setAdmin} = useAdmin();
     else if(usuario instanceof Estudiante){
       //  notificaciones = admin.buscarEstudiante(user.id).notificaciones;
       notificaciones = user.notificaciones;
+    
     }
   
     else if(usuario instanceof Creador){
       //suponemos que es creador
      // notificaciones = admin.buscarCreador(user.id).notificaciones;
      notificaciones = user.notificaciones;
+    
   
     }
+    
   }
-  
+
+  const checkNumber = () => {
+    if(notificaciones.length != 0){
+      setNot(notificaciones.length);
+    }
+  }
+ 
  
   return (
     <>
@@ -54,14 +67,18 @@ const {admin, setAdmin} = useAdmin();
       <Layout>
        
         <div className='cajaDerecha'>
+          <>
           <br></br>
-        <h2><strong>Tus Notificaciones: </strong> {notificaciones.length != 0 ? " tienes  varias sin leer": "no hay nada nuevo por aqui"}</h2>
+        
+        <h2><strong>Tus Notificaciones: </strong>  </h2>
         <div>
           <>
           {notificaciones.map((n:Notificacion) => (n != null ? <Desplegable N={n}></Desplegable>: " "))}
           </>
         </div>
         <br></br>
+          </>
+        
         </div>
          
       </Layout>
