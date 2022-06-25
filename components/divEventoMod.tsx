ed27f39b85553c33cae970dc6c6a6be76d575429
juan <jcortesj@unal.edu.co>
2022-06-25 16:12:28 -0500

    pendiente implementar borrar y mod evento

---
+++ components/divEventoMod.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Evento } from '../data/Evento';
+import { useRef,  useLayoutEffect } from 'react';
+type E ={E:Evento};
+let abierto = false;
+const DivMod = ({E}:E) => {
+    
+const deOc = useRef<HTMLDivElement>(null); 
+const tarjetaEvento = useRef<HTMLDivElement>(null);
+
+const descrip = useRef<HTMLParagraphElement>(null); 
+//const [cambiarTexto,setCambiarTexto] = useState("Vaya, que vacio...");
+
+const mostrarDescripcion = ()=>{
+    console.log("boton pulsado");
+  
+    
+       //const _deOc = document.getElementById('deOc');
+       if(abierto==false) {
+           //deOc!.style.display = "block";
+           
+           if(deOc.current !== null && descrip.current !== null){
+  //             setCambiarTexto(E.getDescripcion());
+               descrip.current.innerHTML = E.getDescripcion();
+               deOc.current.style.display = "block";
+               
+               console.log("deberia mostrarme");
+               abierto = true;
+           }
+           //txt_desc!.innerHTML = ev.getDescripcion();
+           
+          
+       }
+       else{
+           if(deOc.current !== null && descrip.current !== null){
+               deOc.current.style.display = "none";
+               console.log(deOc.current + "xd");
+               abierto = false;
+           }
+
+       }
+   }
+ 
+  
+    //usamos el state, y añadimos el set para modificar este.
+  return (
+    <>
+    <div className='mostrarEvento' ref={tarjetaEvento}>
+        <p>{E.getNombre()} ID: {E.getId()}</p>
+        <button className='botonInfo' onClick={mostrarDescripcion}>Detalles</button>
+        <button className='botonInfo' onClick={mostrarDescripcion}>Borrar</button>
+
+     
+    </div>
+    <div className='descripcionOculta' ref={deOc}>
+        <p ref={descrip} ></p>
+        <button onClick={mostrarDescripcion}>Cerrar</button>
+    </div>
+    </>
+  )
+}
+
+
+
+
+
+
+export default DivMod;
